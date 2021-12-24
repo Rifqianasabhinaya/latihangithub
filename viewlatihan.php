87718b00d0d56b245254e710752ae227dc382229
Rifqianasabhinaya <90910950+Rifqianasabhinaya@users.noreply.github.com>
2021-12-24 14:22:45 +0700

    Add files via upload

---
+++ viewlatihan.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    nilai 1 = <?> $nilai1; ?>
+    nilai 2 = <?> $nilai2; ?>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
+
+
